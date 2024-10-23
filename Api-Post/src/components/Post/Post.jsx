37a9c373207d4df6c334e7095d45029125b3e4c9
MH-Kniloy <web.kniloy@gmail.com>
2024-10-24 02:05:37 +0600

    read more btn added

--- Api-Post/src/components/Post/Post.jsx
+++ Api-Post/src/components/Post/Post.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Post = ({title, id, body}) => {
+
+  const [readMore, setReadMore] = useState(false)
+
+  if(!readMore){
+    body = body.substring(0,70) + '....'
+  }
 
   return (
     <>
@@ -9,6 +15,9 @@ const Post = ({title, id, body}) => {
           <h2>{title}</h2>
           <h3>Post No. {id}</h3>
           <p>{body}</p>
+          <button onClick={()=>{
+            !readMore?setReadMore(true):setReadMore(false)
+          }}>{readMore?'Less':'Read More'}</button>
         </div>
       </div>
       
